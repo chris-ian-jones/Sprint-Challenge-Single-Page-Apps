b78a0ace671f1c7c4ff49326c005453dc7c6924f
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-26 17:45:21 +0100

    WIP: declared 3 routes with <Route> components for existing components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Header from './components/Header.js';
 import AppRouter from './components/AppRouter.js';
 import CharacterList from './components/CharacterList'
 import LocationsList from './components/LocationsList.js';
-import EpisodeList from './components/EpisodeList'
+import EpisodeList from './components/EpisodeList';
+
+import { Route } from 'react-router-dom'
+
 
 export default function App() {
   return <main>
     <Header />
     <TabNav />
     <AppRouter />
-    <CharacterList />
-    <LocationsList />
-    <EpisodeList />
+    <Route path="/characters" component={CharacterList} />
+    <Route path="/locations" component={LocationsList} />
+    <Route path="/episodes" component={EpisodeList} />
   </main>
 }
