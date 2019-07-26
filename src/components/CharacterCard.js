38d785f7468353e09d6ebcb8c2801264c7593e51
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-26 16:43:15 +0100

    WIP: setup character card, passed data in via props, need to add styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import { Divider } from 'semantic-ui-react';
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>)
-}
+// export default function CharacterCard ({ props }) {
+//   return (
+//     <div>
+//       <p>name: {props.name}</p>
+//       <p>status: {props.status}</p>
+//       <p>location: {props.location}</p>
+//       <p>episodes: {props.epsiodes}</p>
+//       <p>image: {props.image}</p>
+//     </div>
+  
+//     )
+// }
+
+const CharacterCard = (props) => (
+  <div>
+  <p>name: {props.name}</p>
+         <p>status: {props.status}</p>
+         <p>location: {props.location}</p>
+         <p>episodes: </p>
+         <p>image: {props.image}</p>
+       </div>
+)
+
+export default CharacterCard
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -18,9 +19,20 @@ export default function CharacterList() {
 
   }, [])
 
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+  return (
+  
+    <section className='character-list grid-view'>
+      {data.map(character => 
+        <CharacterCard 
+        key={character.id}
+        name={character.name}
+        status={character.status}
+        location={character.location.name}
+        episodes={character.episodes}
+        image={character.image}
+        />
+        )}
     </section>
-
+    
+    );
 }
